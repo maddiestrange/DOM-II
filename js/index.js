86c9db02e989cc056bd1a969048b9a9d11e4815f
maddiestrange <maddie.strange@gmail.com>
2019-06-18 14:12:16 -0500

    added click event listener to nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let navLinks = document.querySelector("nav");
+navLinks.addEventListener('click', function(event){
+    console.log("clicked nav link!");
+    event.target.style.color = 'red';
+});
+
